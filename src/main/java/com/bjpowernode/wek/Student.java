33e527dc8094a63d7997f4492035599752dfb57f
wangerkang <1502398746@qq.com>
2022-06-06 17:48:06 +0800

    first test

---
+++ src/main/java/com/bjpowernode/wek/Student.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.wek;
+
+/**
+ * @auther wangerkang
+ * @date 2022/6/6 17:45
+ */
+public class Student {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
