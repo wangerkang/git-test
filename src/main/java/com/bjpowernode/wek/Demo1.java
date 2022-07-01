19b5cc328d9d0cf4220a00ff280d47a127b825ea
wangerkang <1502398746@qq.com>
2022-07-01 09:22:06 +0800

    xinjianDemo1

---
+++ src/main/java/com/bjpowernode/wek/Demo1.java
@@ -0,0 +1,12 @@
+package com.bjpowernode.wek;
+
+/**
+ * @auther wangerkang
+ * @date 2022/7/1 9:20
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("wek");
+    }
+}
+
